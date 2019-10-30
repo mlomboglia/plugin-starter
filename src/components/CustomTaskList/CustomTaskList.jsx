75289367dfdbf95ccae56ac9c0c4fd1e0f937df4
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2019-10-30 10:08:12 +0000

    Changed text in component

--- src/components/CustomTaskList/CustomTaskList.jsx
+++ src/components/CustomTaskList/CustomTaskList.jsx
@@ -10,7 +10,7 @@ const CustomTaskList = (props) => {
 
   return (
     <CustomTaskListComponentStyles>
-      This is a changed demo component
+      This is a new demo component
       <i className="accented" onClick={props.dismissBar}>
         close
       </i>
