a32f3ebd80eb19b839835e03773938b2d9415d19
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2019-10-30 11:43:30 +0000

    Test Flow

--- src/components/CustomTaskList/CustomTaskList.jsx
+++ src/components/CustomTaskList/CustomTaskList.jsx
@@ -10,7 +10,7 @@ const CustomTaskList = (props) => {
 
   return (
     <CustomTaskListComponentStyles>
-      This is a flow demo component
+      This is a flow demo component -- Marcos test
       <i className="accented" onClick={props.dismissBar}>
         close
       </i>
