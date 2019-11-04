d38914ea5cd75698153c445472458e75c23c4f3b
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2019-11-04 15:38:50 +0000

    Typo

--- src/components/CustomTaskList/CustomTaskList.jsx
+++ src/components/CustomTaskList/CustomTaskList.jsx
@@ -10,7 +10,7 @@ const CustomTaskList = (props) => {
 
   return (
     <CustomTaskListComponentStyles>
-      This is a flow demo component -- Marcos
+      This is a flow demo component - Marcos
       <i className="accented" onClick={props.dismissBar}>
         close
       </i>
